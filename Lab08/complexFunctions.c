3b82ffc6ce7470de8f6d3ed3543682e788598f69
SunsetMichi <56726346+SunsetMichi@users.noreply.github.com>
2019-12-07 20:14:30 +1100

    Create complexFunctions.c

---
+++ Lab08/complexFunctions.c
@@ -0,0 +1,93 @@
+// Calculate the magnitude of a complex number
+// By Megan Wong (z5241209)
+// Modified on 2019-07-03
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// DO NOT CHANGE THIS STRUCT
+typedef struct _complex {
+    double real;
+    double imaginary;
+} complex;
+
+complex complexAdd(complex a, complex b);
+complex complexMultiply(complex a, complex b);
+complex complexSquare(complex c);
+
+// DO NOT CHANGE THIS MAIN FUNCTION
+int main (int argc, char *argv[]) {
+
+    complex complex1;
+    complex complex2;
+
+    printf("Enter the real part of the first number: ");
+    scanf("%lf", &complex1.real);
+
+    printf("Enter the imaginary part of the first number: ");
+    scanf("%lf", &complex1.imaginary);
+
+    printf("Enter the real part of the second number: ");
+    scanf("%lf", &complex2.real);
+
+    printf("Enter the imaginary part of the second number: ");
+    scanf("%lf", &complex2.imaginary);
+
+    // Calculate the sum
+    complex added = complexAdd(complex1, complex2);
+
+    // Print the sum.
+    printf("The sum is %.2lf + %.2lfi.\n",
+        added.real, added.imaginary);
+
+    // Calculate the product.
+    complex multiplied = complexMultiply(complex1, complex2);
+
+    // Print the product.
+    printf("The product is %.2lf + %.2lfi.\n",
+        multiplied.real, multiplied.imaginary);
+
+    // Calculate the square of the first complex number.
+    complex squared = complexSquare(complex1);
+
+    // Print it out.
+    printf("The square of the first number is %.2lf + %.2lfi.\n",
+        squared.real, squared.imaginary);
+
+    return EXIT_SUCCESS;
+}
+// END OF MAIN FUNCTION
+
+// Calculate the sum of two complex numbers.
+complex complexAdd(complex a, complex b) {
+    // PUT YOUR CODE HERE;
+    complex sum;
+    sum.real = a.real + b.real;
+    sum.imaginary = a.imaginary + b.imaginary;
+    // = (a.real + b.real)+(a.imaginary + b.imaginary);
+
+    return sum; // CHANGE THIS TO YOUR RETURN VALUE
+}
+
+// Calculate the product of two complex numbers.
+complex complexMultiply(complex a, complex b) {
+    // PUT YOUR CODE HERE;
+    complex product;
+    product.real = (a.real*b.real) - (a.imaginary*b.imaginary);
+    product.imaginary = (a.imaginary*b.real + a.real*b.imaginary);
+    // = (a.real*b.real) - (a.imaginary*b.imaginary) + (a.imaginary*b.real + a.real*b.imaginary);
+
+    return product; // CHANGE THIS TO YOUR RETURN VALUE
+}
+
+// Calculate the square of a complex number.
+complex complexSquare(complex c) {
+    // PUT YOUR CODE HERE;
+    complex square;
+    square.real = (c.real*c.real) - (c.imaginary*c.imaginary);
+    square.imaginary = 2*c.real*c.imaginary;
+    //square = (c.real*c.real) - (c.imaginary*c.imaginary) + (2*c.real*c.imaginary);
+
+    return square; // CHANGE THIS TO YOUR RETURN VALUE
+}
